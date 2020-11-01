2060a872b6c484124f5cc7365e1015ab940c170e
Autif Kamal <kautif@gmail.com>
2020-11-01 08:42:08 -0700

    added user and poll models

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
+
+const db = require('./models');
 
 const handler = require('./handlers');
 
@@ -12,6 +17,8 @@ app.listen(port, console.log(`Listening on port ${port}`));
 app.get('/', (req, res) => {
     res.json({greet: 'hello'});
 })
+
+app.use(cors);
 
 app.use(handler.statusHandler);
 
